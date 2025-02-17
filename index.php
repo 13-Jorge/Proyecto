fbfd2aa4c4e3caeb3a1dd2285b64f5976c36dd47
Jorge1489 <jorge.alvarez1489@gmail.com>
2025-02-17 12:27:37 +0100

    ArrefloIndex2

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+session_start();
+include_once 'connectDB/connect.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CM Gestión Inmobiliaria - Tu Socio en Bienes Raíces</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <script src="js/cerrarSesion.js"></script>
+    <link rel="stylesheet" href="styles/index.css">
+</head>
+
+<body>
+    <!-- Header con la barra de navegación -->
 <header>
         <nav class="navbar navbar-expand-lg navbar-light fixed-top">
             <div class="container">
@@ -149,6 +169,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="js/cerrarSesion.js"></script>
     <script src="js/contacto.js"></script>
 </body>
 
