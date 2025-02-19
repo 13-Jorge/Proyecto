58ce92129aa6e5355b4aaadd9bded5d7d8f4343e
Jorge1489 <jorge.alvarez1489@gmail.com>
2025-02-19 09:53:14 +0100

    borradoComentario

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    // Smooth scrolling for anchor links
     $('a[href^="#"]').on('click', function(event) {
         var target = $(this.getAttribute('href'));
         if( target.length ) {
